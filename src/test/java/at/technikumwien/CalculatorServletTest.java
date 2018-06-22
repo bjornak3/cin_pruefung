1d4d9a43a11762bc49188d0f5e457f751a4736dd
bjorna <bjornak3@gmail.com>
2018-06-22 14:11:46 +0200

    v2

--- src/test/java/at/technikumwien/CalculatorServletTest.java
+++ src/test/java/at/technikumwien/CalculatorServletTest.java
@@ -1,8 +1,55 @@
 package at.technikumwien;
 
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+import com.meterware.servletunit.ServletRunner;
+import com.meterware.servletunit.ServletUnitClient;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import static org.junit.Assert.assertEquals;
 
 @Category(IntegrationTest.class)
 public class CalculatorServletTest {
-	// TODO 3 sinnvolle Integrationstests mittels HttpUnit ergänzen
+	// TODO 3 sinnvolle Integrationstests mittels HttpUnit ergï¿½nzen
+
+    private ServletRunner runner;
+    private ServletUnitClient client;
+
+    @Before
+    public void setUp(){
+        runner = new ServletRunner();
+        runner.registerServlet("calculator", CalculatorServlet.class.getName());
+
+        client = runner.newClient();
+    }
+
+
+    @Test
+    public void testSumme() throws Exception{
+        WebRequest request = new GetMethodWebRequest("http://test.meterware.com/calculator");
+
+        request.setParameter("operand1", "2");
+        request.setParameter("operand2", "3");
+        System.out.println(request);
+        WebResponse response = client.getResponse(request);
+       assertEquals("Summe: 5", response.getText() );
+    }
+
+    @Test (expected = NumberFormatException.class)
+    public void testUnsupportedOperand() throws Exception{
+        WebRequest request = new GetMethodWebRequest("http://test.meterware.com/calculator");
+        request.setParameter("operand1", "abc");
+
+        WebResponse response = client.getResponse(request);
+    }
+
+    @Test (expected = NumberFormatException.class)
+    public void testEmptyRequest() throws Exception{
+        WebRequest request = new GetMethodWebRequest("http://test.meterware.com/calculator");
+
+        WebResponse response = client.getResponse(request);
+
+    }
 }
--- src/test/java/at/technikumwien/CalculatorTest.java
+++ src/test/java/at/technikumwien/CalculatorTest.java
@@ -3,6 +3,8 @@ package at.technikumwien;
 import org.junit.Before;
 
 import at.technikumwien.Calculator;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class CalculatorTest {
 	private Calculator calculator;
@@ -12,5 +14,20 @@ public class CalculatorTest {
 		calculator = new Calculator();		
 	}
 	
-	// TODO 3 sinnvolle Unittests ergänzen
+	// TODO 3 sinnvolle Unittests ergï¿½nzen
+
+	@Test
+	public void testWithParams(){
+		assertEquals(5,calculator.sum(2,3));
+	}
+
+	@Test
+	public void testWithNull(){
+		assertEquals(0,calculator.sum(null));
+	}
+
+	@Test
+	public void testWithNoParams(){
+		assertEquals(0, calculator.sum());
+	}
 }
