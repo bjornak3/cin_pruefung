6a7fe3bf3243824043f120d2aaca4606cf7a1c1e
bjorna <bjornak3@gmail.com>
2018-06-22 13:40:44 +0200

    v1!

---
+++ src/test/java/at/technikumwien/CalculatorServletTest.java
@@ -0,0 +1,8 @@
+package at.technikumwien;
+
+import org.junit.experimental.categories.Category;
+
+@Category(IntegrationTest.class)
+public class CalculatorServletTest {
+	// TODO 3 sinnvolle Integrationstests mittels HttpUnit ergänzen
+}
---
+++ src/test/java/at/technikumwien/CalculatorTest.java
@@ -0,0 +1,16 @@
+package at.technikumwien;
+
+import org.junit.Before;
+
+import at.technikumwien.Calculator;
+
+public class CalculatorTest {
+	private Calculator calculator;
+	
+	@Before
+	public void setUp() {
+		calculator = new Calculator();		
+	}
+	
+	// TODO 3 sinnvolle Unittests ergänzen
+}
